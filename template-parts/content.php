0b7e31d0b4ed41c27bec828e07c3cf2b5333d428
Kweku Moses <kweku.moses@hotmail.com>
2022-02-15 08:56:00 +0100

    add some styles :eye_speech_bubble:

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,22 +13,26 @@
 
     <!-- background image for hero  -->
     <!-- here is used an inline style attribute to attach the hero image as a css background directly onto the element -->
-    <section class="hero" style="
+
+    <!-- make the hero have a min height of 100vh, cover the background, center it, add flexbox and make the content center vertically and horizontally  -->
+    <section class="hero min-vh-100 cover bg-center flex items-center justify-center" style="
 	  <?php if (get_field('hero_image')): ?>
 			background-image: url(<?php the_field('hero_image'); ?>)
 				<?php endif; ?>
 				">
-        <div class="hero-content">
+        <!-- make text white and center it -->
+        <div class="hero-content white tc">
             <!-- the location title  -->
-            <h1> <?php the_title(); ?> </h1>
+            <h1 class="bold ma0 ttu hero-heading b"> <?php the_title(); ?> </h1>
             <!-- the location subheading -->
             <?php if (get_field('subhead')): ?>
-            <p><?php the_field('subhead'); ?></p>
+
+            <p class="hero-subhead mb6 mt0 ttu"><?php the_field('subhead'); ?></p>
             <?php endif; ?>
 
             <!-- formatted date  -->
             <?php if (get_field('date')): ?>
-            <p>
+            <p class="f6 date ma0 ttu tracked">
                 <?php 
 			// convert  date into format that php can work with
 			// then format it to be nice + readable
@@ -40,7 +44,7 @@
         </div>
     </section>
 
-    <header class="entry-header">
+    <header class=" entry-header">
 
     </header><!-- .entry-header -->
 
