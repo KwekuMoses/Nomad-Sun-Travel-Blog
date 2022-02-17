a82540027a25cae61d4fad2a33bb6f45526feee5
Kweku Moses <kweku.moses@hotmail.com>
2022-02-17 14:48:18 +0100

    basics done

--- header.php
+++ header.php
@@ -17,7 +17,6 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="stylesheet" href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
--- template-parts/gallery.php
+++ template-parts/gallery.php
@@ -4,7 +4,7 @@
     <?php foreach($images as $image) : ?>
     <!-- here is the header backrgroundimage -->
     <!-- here we have added a custom field to our attachments which adds a class name to control the width of the images -->
-    <div class="gallery-image ph3 mb4  <?php the_field('image_width', $image['id']) ?>">
+    <div class="gallery-image ph3 mb4  <?php the_field('image_width', $image['id']) ?> ">
         <!-- make the font 50% opacity -->
         <!-- get image by ID -->
         <?php echo wp_get_attachment_image($image['id'], 'full'); ?>
